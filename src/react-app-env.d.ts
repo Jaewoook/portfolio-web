815538ac1685446dd8d105c66bf5c8d001eda6e0
Jaewook Ahn <ajw4586@gmail.com>
2020-06-16 00:58:18 +0900

    Add support to inject NODE_ENV in window object

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+interface Window {
+    __ENV__: {
+        NODE_ENV: string;
+    };
+}
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+const getEnv = () => {
+    return window.__ENV__.NODE_ENV;
+}
+
+export const log = {
+
+};
