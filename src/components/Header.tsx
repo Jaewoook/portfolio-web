98d6f6b9506b4ab57e0823bb2de0769288d9d454
Jaewook Ahn <ajw4586@gmail.com>
2020-07-14 03:19:35 +0900

    Apply social link in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components/macro";
 import { display, DisplayProps, zIndex, ZIndexProps } from "styled-system";
 import { Button, Layout } from "antd";
+import * as urls from "../assets/urls";
 
 const HeaderWrapper = styled(Layout.Header)<DisplayProps & ZIndexProps>`
     width: 100%;
@@ -21,7 +22,7 @@ HeaderWrapper.defaultProps = {
 
 export const Header: React.FC<DisplayProps> = (props) => {
     return <HeaderWrapper {...props}>
-        <Button type="link">GitHub</Button>
-        <Button type="link">Blog</Button>
+        <Button type="link" href={urls.github}>GitHub</Button>
+        <Button type="link" href={urls.blog}>Blog</Button>
     </HeaderWrapper>;
 };
