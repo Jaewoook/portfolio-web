d41be3fb1d80065c48f60dab974ca8e8dc294376
Jaewook Ahn <dev.jaewook@gmail.com>
2023-12-06 20:21:17 +0900

    chore(lib): add vanilla-extract support
    - add vanilla-extract dependencies
    - remove styled dependencies
    - remove support module for styled-components

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 // @ts-check
 
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+
 /**
  * @type {import("next").NextConfig}
  */
 const config = {
-
+  crossOrigin: "anonymous",
 };
 
-module.exports = config;
+module.exports = withVanillaExtract(config);
