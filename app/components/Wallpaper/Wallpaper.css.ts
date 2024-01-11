f2041942913f4a288daeb68e17c1160bc924b2c4
Jaewook Ahn <dev.jaewook@gmail.com>
2024-01-11 14:30:25 +0900

    refactor(wallpaper): set absolute position for Wallpaper

--- app/components/Wallpaper/Wallpaper.css.ts
+++ app/components/Wallpaper/Wallpaper.css.ts
@@ -3,6 +3,8 @@ import { style } from "@vanilla-extract/css";
 export const container = style({
   width: "100%",
   height: "100%",
+  position: "absolute",
+  inset: 0,
 });
 
 export const image = style({
