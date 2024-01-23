0d284d5fea8c2796d3af3fff94030ca86ad3d694
Jaewook Ahn <dev.jaewook@gmail.com>
2024-01-23 14:33:18 +0900

    feat(seo): add favicon
    - add web manifest file

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,10 @@ const RootLayout = ({ children }: PropsWithChildren) => {
         <meta name="og:url" content="https://portfolio.jaewook.me" />
         <meta name="og:image" content="https://portfolio.jaewook.me/thumbnail.png" />
         <meta name="og:locale" content="en_US" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@200;300;400;500;600;700;800;900&display=swap"
